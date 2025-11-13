4ec89f8c0eb0cd919c75f5e3262b673727c2c180
rahulkumar-9997 <rahulkumarmaurya464@gmail.com>
2025-11-13 16:23:29 +0530

    13_11_2025_update_g_login

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -5,14 +5,15 @@
 		@stack('styles')
 	</head>
     <body class="bg-effect">
-		@include('frontend.layouts.header-menu')
 		
-		@yield('main-content')
-	
+		@include('frontend.layouts.header-menu')		
+		@yield('main-content')	
 		@include('frontend.layouts.footer')
 		<!-- @include('frontend.pages.partials.cart-drawer') -->
 		@stack('schema')
 		@include('frontend.layouts.footerjs')
 		@stack('scripts')
+		
+
 	</body>
 </html>

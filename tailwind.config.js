f928b97639e69560aadc4d76242bbb9a71ce5eda
rahulkumar-9997 <rahulkumarmaurya464@gmail.com>
2026-05-07 14:37:08 +0530

    home page structure and  update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     './resources/js/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          teal: "#0F8B8D",
+          navy: "#062B45",
+          mint: "#DFF4F2",
+          cyan: "#1AA6A5",
+        },
+
+        background: {
+          main: "#F8FAFA",
+          card: "#FFFFFF",
+          light: "#EEF7F6",
+        },
+
+        textcolor: {
+          primary: "#1B1B1B",
+          secondary: "#6B7280",
+          light: "#9CA3AF",
+        },
+      },
+    },
   },
   plugins: [],
 }
